765d6a50f55f398024369b1a711e2791f6cd5e83
James Cockbain <james.cockbain@ibm.com>
2019-08-24 12:25:21 +0100

    added solution for two-sum

---
+++ two-sum.py
@@ -0,0 +1,11 @@
+def two_sum(nums, target):
+    d = {}
+    for i, n in enumerate(nums):
+        m = target - n
+        if m in d:
+            return [d[m], i]
+        else:
+            d[n] = i
+
+
+print(two_sum([1, 2, 4, 5], 9))
