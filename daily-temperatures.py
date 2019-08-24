8807c85ae47cd2e61826230e88b4f99de1d872c3
James Cockbain <james.cockbain@ibm.com>
2019-08-24 15:31:54 +0100

    added stack solution for daily temps

---
+++ daily-temperatures.py
@@ -0,0 +1,27 @@
+# Time O(NW)
+# Space O(N + W)
+def dailyTemperatures(T):
+    nxt = [float('inf')] * 102
+    ans = [0] * len(T)
+    for i in range(len(T) - 1, -1, -1):
+        warmer_index = min(nxt[t] for t in range(T[i]+1, 102))
+        if warmer_index < float('inf'):
+            ans[i] = warmer_index - i
+        nxt[T[i]] = i
+    return ans
+
+# Time O(N)
+# Space O(W)
+def dailyTemperatures2(T):
+    ans = [0] * len(T)
+    stack = []  # indexes from hottest to coldest
+    for i in xrange(len(T) - 1, -1, -1):
+        while stack and T[i] >= T[stack[-1]]:
+            stack.pop()
+        if stack:
+            ans[i] = stack[-1] - i
+        stack.append(i)
+    return ans
+
+
+print(dailyTemperatures([10, 20, 15, 28, 12, 17]))
