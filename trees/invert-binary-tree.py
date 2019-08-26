b67fe95fc4d61765ec17b7e8f50b141a0149cc43
James Cockbain <james.cockbain@ibm.com>
2019-08-26 10:58:06 +0100

    added diameter of binary tree

---
+++ trees/invert-binary-tree.py
@@ -0,0 +1,18 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root is None:
+            return None
+        root.left, root.right = \
+            self.invertTree(root.right), self.invertTree(root.left)
+        return root
--- trees/tree-depth.py
+++ trees/tree-depth.py
@@ -1,7 +1,6 @@
 def maxDepth(self, root):
     if not root:
         return 0
-
     return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
 
