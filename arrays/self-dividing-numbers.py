5678049cfa9a88fea0c91a0f730330e15bef6ea5
James Cockbain <james.cockbain@ibm.com>
2019-08-27 12:10:10 +0100

    added second solution to self-divising numbers

--- arrays/self-dividing-numbers.py
+++ arrays/self-dividing-numbers.py
@@ -14,3 +14,18 @@ def selfDividingNumbers(left, right):
             if c == len(str(i)) - 1:
                 selfDividers.append(i)
     return selfDividers
+
+
+def selfDividingNumbers2(self, left, right):
+    ans = []
+    for n in range(left, right + 1):
+        if self_dividing(n):
+            ans.append(n)
+    return StopAsyncIteration
+
+
+def self_dividing(n):
+    for d in str(n):
+        if d == '0' or n % int(d) > 0:
+            return False
+    return True
