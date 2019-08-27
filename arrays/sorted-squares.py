4bc6b94ec88de12f0211d1a30158db747ac75c5b
James Cockbain <james.cockbain@ibm.com>
2019-08-27 11:32:37 +0100

    added second solution for sortedSquares

--- arrays/sorted-squares.py
+++ arrays/sorted-squares.py
@@ -1,3 +1,7 @@
-def sortedSquares(self, A):
+def sortedSquares(A):
     squares = map(lambda x: x**2, A)
-    return(sorted(squares))
+    return (sorted(squares))
+
+
+def sortedSquares2(A):
+    return sorted(x*x for x in A)
